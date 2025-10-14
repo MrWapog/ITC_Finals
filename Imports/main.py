5920a801cbe23a31f33a5d78ee10a2a090d3b265
MrWapog <zapantarc@students.nu-dasma.edu.ph>
2025-10-14 22:00:39 +0800

    LAnghya

--- Imports/main.py
+++ Imports/main.py
@@ -1,3 +1,4 @@
+import math
 import turtle
 
 SCREEN_WIDTH = 800
@@ -16,8 +17,8 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 
-class Sprite():
-    # THIs is the Contractor
+class Sprite:
+    # This is the Constructor
     def __init__(self, x, y, shape, color):
         self.x = x
         self.y = y
@@ -25,22 +26,67 @@ class Sprite():
         self.color = color
         self.dx = 0
         self.dy = 0
+        self.heading = 0
+        self.da = 0
+        self.thrust = 0.0
+        # reduce acceleration so it isn’t too high
+        self.acceleration = 0.2
 
     def update(self):
+
+        self.heading += self.da
+        self.heading %= 360
+
+        self.dx += math.cos(math.radians(self.heading)) * self.thrust
+        self.dy += math.sin(math.radians(self.heading)) * self.thrust
+
         self.x += self.dx
         self.y += self.dy
 
     def render(self, pen):
         pen.goto(self.x, self.y)
+        pen.setheading(self.heading)
         pen.shape(self.shape)
         pen.color(self.color)
         pen.stamp()
 
-# Create player sprite
-player = Sprite(0, 0, "triangle", "white")
-player.dx = 1
-player.dy = 0.5
+class Player(Sprite):
+    def __init__(self, x, y, shape, color):
+        Sprite.__init__(self, 0, 0, shape, color)
+        self.lives = 3
+        self.score = 0
+        self.heading = 90
+        self.da = 0
 
+    def rotate_left(self):
+        self.da = 5
+
+    def rotate_right(self):
+        self.da = -5
+
+    def stop_rotation(self):
+        self.da = 0
+
+    def accelerate(self):
+        self.thrust += self.acceleration
+
+    def decelerate(self):
+        self.thrust = 0.0
+
+    def render(self, pen):
+        pen.shapesize(0.5, 1.0, None)
+        pen.goto(self.x, self.y)
+        pen.setheading(self.heading)
+        pen.shape(self.shape)
+        pen.color(self.color)
+        pen.stamp()
+
+        pen.shapesize(1.0, 1.0, None)
+
+# Create player sprite
+player = Player(0, 0, "triangle", "white")
+
+# Create other sprites
 enemy = Sprite(0, 100, "square", "red")
 enemy.dx = -1
 enemy.dy = -0.3
@@ -49,28 +95,31 @@ powerup = Sprite(0, -100, "circle", "blue")
 powerup.dy = 1
 powerup.dx = 0.1
 
-#Sprites List
-sprites = []
-sprites.append(player)
-sprites.append(enemy)
-sprites.append(powerup)
+# Sprites List
+sprites = [player, enemy, powerup]
 
-#Where the magic starts here: MAIN LOOP!
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(player.rotate_left, "Left")
+wn.onkeypress(player.rotate_right, "Right")
+
+wn.onkeyrelease(player.stop_rotation, "Left")
+wn.onkeyrelease(player.stop_rotation, "Right")
+
+wn.onkeypress(player.accelerate, "Up")
+wn.onkeyrelease(player.decelerate, "Up")
+
+# MAIN LOOP
 while True:
-    #clear screen
     pen.clear()
 
-    #GaMeStuff
-    #Update sprites
+    # Update sprites
     for sprite in sprites:
         sprite.update()
 
-    #render sprites
+    # Render sprites
     for sprite in sprites:
         sprite.render(pen)
 
-    #UpdateScreen
+    # Update screen
     wn.update()
-
-
-
