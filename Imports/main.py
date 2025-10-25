f0288d1ba10780980ed3072198333a7a5cc68a42
MrWapog <zapantarc@students.nu-dasma.edu.ph>
2025-10-25 15:40:54 +0800

    Just update of the game

--- Imports/main.py
+++ Imports/main.py
@@ -55,6 +55,17 @@ class Sprite:
         self.acceleration = 0.01
         self.health = 100
         self.max_health = 100
+        self.width = 20
+        self.height = 20
+
+    def is_collision(self, other):
+        if self.x < other.x + other.width and\
+            self.x + self.width > other.x and\
+            self.y < other.y + other.height and\
+            self.y + self.height > other.y:
+            return True
+        else:
+            return False
 
     def update(self):
         self.heading += self.da
@@ -136,6 +147,9 @@ class Player(Sprite):
     def decelerate(self):
         self.thrust = 0.0
 
+    def fire(self):
+        missile.fire(self.x, self.y, self.heading, self.dx, self.dy)
+
     def render(self, pen):
         pen.shapesize(0.5, 1.0, None)
         pen.goto(self.x, self.y)
@@ -144,35 +158,117 @@ class Player(Sprite):
         pen.color(self.color)
         pen.stamp()
         pen.shapesize(1.0, 1.0, None)
+
         self.render_health_meter(pen)
 
+#Missile
+class Missile(Sprite):
+    def __init__(self,x, y, shape, color):
+        Sprite.__init__(self, x, y, shape, color)
+        self.state = "ready"
+        self.thrust = 8.0
+        self.max_fuel = 200
+        self.fuel = self.max_fuel
+        self.height = 4
+        self.width = 4
+
+    def fire(self, x, y, heading, dx, dy):
+        if self.state == "ready":
+            self.state = "active"
+            self.x = x
+            self.y = y
+            self.heading = heading
+            self.dx = dx
+            self.dy = dy
+
+            self.dx += math.cos(math.radians(self.heading)) * self.thrust
+            self.dy += math.sin(math.radians(self.heading)) * self.thrust
+
+    def update(self):
+        if self.state == "active":
+            self.fuel -= self.thrust
+            if self.fuel <= 0:
+                self.reset()
+
+            self.heading += self.da
+            self.heading %= 360
+
+            self.x += self.dx
+            self.y += self.dy
+
+            self.border_check()
+
+    def reset(self):
+        self.fuel = self.max_fuel
+        self.dx = 0
+        self.dy = 0
+        self.state = "ready"
+
+    def render(self, pen):
+        if self.state == "active":
+            pen.shapesize(0.2, 0.2, None)
+            pen.goto(self.x, self.y)
+            pen.setheading(self.heading)
+            pen.shape(self.shape)
+            pen.color(self.color)
+            pen.stamp()
+            pen.shapesize(1.0, 1.0, None)
+
+class Enemy(Sprite):
+    def __init__(self, x, y, shape, color):
+        Sprite.__init__(self, x, y, shape, color)
+
+class Powerup(Sprite):
+    def __init__(self, x, y, shape, color):
+        Sprite.__init__(self, x, y, shape, color)
 
 # Game object
-game = Game(600, 300)
+game = Game(1000, 300)
 
 # Create player sprite
 player = Player(0, 0, "triangle", "white")
 
+#Crete missile obj
+missile = Missile(0, 100, "circle", "yellow")
+
 # Create other sprites
-enemy = Sprite(0, 100, "square", "red")
+enemy = Enemy(0, 100, "square", "red")
 enemy.dx = -1
 enemy.dy = -0.3
 
-powerup = Sprite(0, -100, "circle", "blue")
+enemy2 = Enemy(-100, 100, "square", "red")
+enemy2.dx = 1
+enemy2.dy = 0.3
+
+powerup = Powerup(0, -100, "circle", "blue")
 powerup.dy = 1
 powerup.dx = 0.1
 
+powerup2 = Powerup(-100, -100, "circle", "blue")
+powerup2.dy = -1
+powerup2.dx = -0.1
+
 # Sprites List
-sprites = [player, enemy, powerup]
+sprites = []
+sprites.append(player)
+sprites.append(enemy)
+sprites.append(powerup)
+sprites.append(missile)
+sprites.append(enemy2)
+sprites.append(powerup2)
 
 # Keyboard bindings
 wn.listen()
 wn.onkeypress(player.rotate_left, "Left")
 wn.onkeypress(player.rotate_right, "Right")
+
 wn.onkeyrelease(player.stop_rotation, "Left")
 wn.onkeyrelease(player.stop_rotation, "Right")
+
 wn.onkeypress(player.accelerate, "Up")
 wn.onkeyrelease(player.decelerate, "Up")
+
+wn.onkeypress(player.fire, "space")
 
 # MAIN LOOP
 while True:
@@ -180,6 +276,28 @@ while True:
 
     for sprite in sprites:
         sprite.update()
+
+    for sprite in sprites:
+        if isinstance(sprite, Enemy):
+            if player.is_collision(sprite):
+                player.x = 0
+                player.y = 0
+
+            if missile.state =="active" and missile.is_collision(sprite):
+                sprite.x = -100
+                sprite.y = -100
+                missile.reset()
+
+        if isinstance(sprite, Powerup):
+            if player. is_collision(sprite):
+                sprite.x = 100
+                sprite.y = 100
+
+            if missile.state =="active" and missile.is_collision(sprite):
+                sprite.x = 100
+                sprite.y = -100
+                missile.reset()
+
 
     for sprite in sprites:
         sprite.render(pen)
